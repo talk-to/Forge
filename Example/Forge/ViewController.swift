2f783d1347e69905c4864e14ccb67431cbb03712
Ayush Goel <ayushgoel111@gmail.com>
2018-12-20 15:43:34 +0530

    Add required files for pod from template

---
+++ Example/Forge/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Forge
+//
+//  Created by Ayush Goel on 12/20/2018.
+//  Copyright (c) 2018 Ayush Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
