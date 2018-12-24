367a8d7b46c6d0074fdf1b497dfbc452d4ab35fc
Ayush Goel <ayushgoel111@gmail.com>
2018-12-24 15:57:01 +0530

    Hide IDType and use string as it creates problem using protocol with associated type without generics

--- Forge/Classes/Task.swift
+++ Forge/Classes/Task.swift
@@ -5,14 +5,14 @@ import Foundation
 // be used in TaskManager.
 // Check https://stackoverflow.com/questions/36348061/protocol-can-only-be-used-as-a-generic-constraint-because-it-has-self-or-associa
 
-public protocol IDType: Codable {
-}
-
-extension String: IDType {}
+//public protocol IDType: Codable, Hashable {
+//}
+//
+//extension String: IDType {}
 
 public protocol Task: Codable {
 
-  init(id: IDType)
-  var id: IDType { get }
+  init(id: String)
+  var id: String { get }
 
 }
