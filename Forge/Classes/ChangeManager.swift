5c054e11a6a41dfe9e9ebe4c4804c9cdc6871fd4
Ayush Goel <ayushgoel111@gmail.com>
2018-12-24 20:14:58 +0530

    Remove didStart callback as not required

--- Forge/Classes/ChangeManager.swift
+++ Forge/Classes/ChangeManager.swift
@@ -4,7 +4,6 @@ import Result
 public protocol ChangeManager: class {
 
   func willStart(task: Task)
-  func didStart(task: Task)
 
   /// Should return @p YES if the task should be retried
   func didComplete(task: Task, result: Result<Bool, ExecutorError>) -> Bool
