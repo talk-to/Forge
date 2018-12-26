68aaa00d39a2e62f6d5f3d5a7ff2c630474173d1
Ayush Goel <ayushgoel111@gmail.com>
2018-12-26 17:16:10 +0530

    Create core data stack in persistor on initialisation

--- Forge/Classes/Persistor.swift
+++ Forge/Classes/Persistor.swift
@@ -1,24 +1,44 @@
 
+import CoreData
+
 class Persistor {
+
+  static func persistentContainer(UUID: String) -> NSPersistentContainer {
+    guard let bundlePath = Bundle(for: Persistor.self).path(forResource: "Forge", ofType: "bundle"), // defined in podspec
+      let forgeBundle = Bundle(path: bundlePath),
+      let modelURL = forgeBundle.url(forResource: "forge", withExtension: "momd") else {
+        fatalError("Failed to get model URL for forge.")
+    }
+    guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
+      fatalError("Failed to create model from model URL for forge.")
+    }
+    return NSPersistentContainer(name: UUID, managedObjectModel: model)
+  }
+
+  let UUID: String
+  let persistentContainer: NSPersistentContainer
+  let context: NSManagedObjectContext
+
+  init(UUID: String) {
+    self.UUID = UUID
+    self.persistentContainer = type(of: self).persistentContainer(UUID: UUID)
+    persistentContainer.loadPersistentStores { (description, error) in
+      if let error = error {
+        fatalError("Failed to load Core Data stack: \(error)")
+      }
+      print("Core data stack for Forge initialised.")
+    }
+    context = persistentContainer.newBackgroundContext()
+  }
+}
+
+extension Persistor {
   func save(task: PersistentTask) {
 
   }
 
-  func completedTasks() -> [PersistentTask] {
-    return []
-  }
-
-  func failedTasks() -> [PersistentTask] {
-    return []
-  }
-
   func tasks(ofType type: String) -> [PersistentTask] {
     return []
-  }
-
-  let UUID: String
-  init(UUID: String) {
-    self.UUID = UUID
   }
 }
 
