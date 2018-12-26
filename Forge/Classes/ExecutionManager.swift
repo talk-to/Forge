546d1c93041fbee9cbe16bc76e8938158d7ecc93
Ayush Goel <ayushgoel111@gmail.com>
2018-12-26 16:04:10 +0530

    Use execution delegate to delete and fail tasks

--- Forge/Classes/ExecutionManager.swift
+++ Forge/Classes/ExecutionManager.swift
@@ -2,29 +2,36 @@
 import Foundation
 import Result
 
+protocol ExecutionDelegate: class {
+  func delete(task: PersistentTask)
+  func fail(task: PersistentTask, increaseRetryCount: Bool)
+}
+
 class ExecutionManager {
 
+  weak var executionDelegate: ExecutionDelegate?
   weak var changeManager: ChangeManager?
+
   func execute(task pTask: PersistentTask) {
     if let executor = executors[pTask.task.id] {
       changeManager?.willStart(task: pTask.task)
       executor.execute(task: pTask.task) { result in
         switch result {
         case .success(_):
+          executionDelegate?.delete(task: pTask)
           changeManager?.didComplete(task: pTask.task, result: result)
         case .failure(let error):
           switch error {
           case .NonRetriable:
-            // remove task from persistor
+            executionDelegate?.delete(task: pTask)
             changeManager?.didComplete(task: pTask.task, result: result)
           case .ConditionalRetriable:
+            executionDelegate?.fail(task: pTask, increaseRetryCount: false)
             // set as failed task
             // retry later
             return
           case .NonConditionalRetriable:
-            // set as failed task
-            // increase tried number
-            // retry later if required, or fail here
+            executionDelegate?.fail(task: pTask, increaseRetryCount: true)
             changeManager?.didComplete(task: pTask.task, result: result) // Possible in only certain cases
           }
         }
