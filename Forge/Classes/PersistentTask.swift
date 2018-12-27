74c41f4fe994421f60e0962a3bb5912047374dfe
Ayush Goel <ayushgoel111@gmail.com>
2018-12-27 18:10:30 +0530

    Remove value transformer for now as not required

--- Forge/Classes/PersistentTask.swift
+++ Forge/Classes/PersistentTask.swift
@@ -10,3 +10,23 @@ struct PersistentTask {
     self.task = task
   }
 }
+
+//class PersistentTaskValueTransformer: ValueTransformer {
+//  override func transformedValue(_ value: Any?) -> Any? {
+//    guard let v = value else {
+//      return nil
+//    }
+//    return NSKeyedArchiver.archivedData(withRootObject: v)
+//  }
+//
+//  override func reverseTransformedValue(_ value: Any?) -> Any? {
+//    guard let v = value as? Data else {
+//      return nil
+//    }
+//    return NSKeyedUnarchiver.unarchiveObject(with: v)
+//  }
+//
+//  override class func allowsReverseTransformation() -> Bool {
+//    return true
+//  }
+//}
