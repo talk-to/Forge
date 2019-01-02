c6c58a14921b2cf3e5e9c5d11296bdef9e7f36d6
Ayush Goel <ayushgoel111@gmail.com>
2019-01-02 16:26:46 +0530

    Add countOfRetries to PersistentTask

--- Forge/Classes/PersistentTask.swift
+++ Forge/Classes/PersistentTask.swift
@@ -3,11 +3,13 @@ import Foundation
 
 struct PersistentTask {
   let uniqueID: String
+  let countOfRetries: Int
   let task: Task
 
   init(task: Task) {
     uniqueID = UUID().uuidString
     self.task = task
+    self.countOfRetries = 0
   }
 }
 
