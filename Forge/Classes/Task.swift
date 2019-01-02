5cc0ef339d5644ff0bc6dca0148803d49fad1b3c
Ayush Goel <ayushgoel111@gmail.com>
2019-01-02 16:30:07 +0530

    Make task a final class that can be serialized as well as deserialized
    Keeping Task as a protocol was a problem because we were not able to deserialize tasks since Decoders need a concrete type for decoding. Protocols are meta types instead of concrete types so we were not able to deserialize.

--- Forge/Classes/Task.swift
+++ Forge/Classes/Task.swift
@@ -1,34 +1,14 @@
 
 import Foundation
 
-// Can not use an associatedType in @p Task since it makes `id` too generic to
-// be used in TaskManager.
-// Check https://stackoverflow.com/questions/36348061/protocol-can-only-be-used-as-a-generic-constraint-because-it-has-self-or-associa
+public final class Task: Codable {
 
-//public protocol IDType: Codable, Hashable {
-//}
-//
-//extension String: IDType {}
+  let id: String
+  let type: String
+  var params: Dictionary<String, String>? = nil
 
-public protocol Task: Codable {
-
-  init(id: String)
-  var id: String { get }
-
-}
-
-fileprivate let encoder = JSONEncoder()
-fileprivate let decoder = JSONDecoder()
-
-fileprivate let encoding = String.Encoding.utf8
-
-extension Task {
-  func stringRepresentation() -> String {
-    let taskData = try! encoder.encode(self)
-    return String(data: taskData, encoding: encoding)!
-  }
-
-  func task(from repr: String) -> Self {
-    return try! decoder.decode(Self.self, from: repr.data(using: encoding)!)
+  init(id: String, type: String) {
+    self.id = id
+    self.type = type
   }
 }
