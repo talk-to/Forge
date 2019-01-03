e15dccf30de9f4665f89517286488a51b38fad88
Ayush Goel <ayushgoel111@gmail.com>
2019-01-03 19:11:07 +0530

    Make Task interface public for modification

--- Forge/Classes/Task.swift
+++ Forge/Classes/Task.swift
@@ -2,11 +2,11 @@
 import Foundation
 
 public final class Task: Codable {
-  let id: String
-  let type: String
-  var params: Dictionary<String, String>? = nil
+  public let id: String
+  public let type: String
+  public var params: Dictionary<String, String>? = nil
 
-  init(id: String, type: String) {
+  public init(id: String, type: String) {
     self.id = id
     self.type = type
   }
