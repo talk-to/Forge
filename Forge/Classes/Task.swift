422ea530644b32d273e9a70c8e04e762fbdfd68e
Ayush Goel <ayushgoel111@gmail.com>
2019-01-04 19:25:21 +0530

    Reenable params dictionary for usage

--- Forge/Classes/Task.swift
+++ Forge/Classes/Task.swift
@@ -4,8 +4,8 @@ import Foundation
 public class Task: Codable {
   public let id: String
   public let type: String
-////  public var params: Dictionary<String, String>? = nil
-//
+  public var params: Dictionary<String, String>? = nil
+
   public init(id: String, type: String) {
     self.id = id
     self.type = type
