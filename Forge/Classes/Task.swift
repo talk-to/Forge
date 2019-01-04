b3dec21e7654788038f8dddff29becc37dc8d021
Ayush Goel <ayushgoel111@gmail.com>
2019-01-04 15:03:22 +0530

    Try to get Task compilable in flockmail

--- Forge/Classes/Task.swift
+++ Forge/Classes/Task.swift
@@ -1,11 +1,11 @@
 
 import Foundation
 
-public final class Task: Codable {
+public class Task: Codable {
   public let id: String
   public let type: String
-  public var params: Dictionary<String, String>? = nil
-
+////  public var params: Dictionary<String, String>? = nil
+//
   public init(id: String, type: String) {
     self.id = id
     self.type = type
