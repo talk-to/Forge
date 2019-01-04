7403a3a9fe5ffc8f1f46ae0e987f4fe300b26643
Ayush Goel <ayushgoel111@gmail.com>
2019-01-04 19:42:32 +0530

    Assert executor should be available when execution is started

--- Forge/Classes/ExecutionManager.swift
+++ Forge/Classes/ExecutionManager.swift
@@ -14,30 +14,32 @@ class ExecutionManager {
   weak var changeManager: ChangeManager?
 
   func execute(task pTask: PersistentTask) {
-    if let executor = executors[pTask.task.id] {
-      changeManager?.willStart(task: pTask.task)
-      executionDelegate?.start(pTask: pTask)
-      executor.execute(task: pTask.task, countOfRetries: pTask.countOfRetries) { [weak self] result in
-        guard let strongSelf = self else {
-          assertionFailure("Execution manager deallocated!")
-          return
-        }
-        switch result {
-        case .success(_):
+    guard let executor = executors[pTask.task.id] else {
+      assertionFailure("Trying to execute without registering an executor")
+      return
+    }
+    changeManager?.willStart(task: pTask.task)
+    executionDelegate?.start(pTask: pTask)
+    executor.execute(task: pTask.task, countOfRetries: pTask.countOfRetries) { [weak self] result in
+      guard let strongSelf = self else {
+        assertionFailure("Execution manager deallocated!")
+        return
+      }
+      switch result {
+      case .success(_):
+        strongSelf.executionDelegate?.delete(pTask: pTask)
+        strongSelf.changeManager?.didComplete(task: pTask.task, result: result)
+      case .failure(let error):
+        switch error {
+        case .NonRetriable:
           strongSelf.executionDelegate?.delete(pTask: pTask)
           strongSelf.changeManager?.didComplete(task: pTask.task, result: result)
-        case .failure(let error):
-          switch error {
-          case .NonRetriable:
-            strongSelf.executionDelegate?.delete(pTask: pTask)
-            strongSelf.changeManager?.didComplete(task: pTask.task, result: result)
-          case .ConditionalRetriable:
-            strongSelf.executionDelegate?.fail(pTask: pTask, increaseRetryCount: false)
-            return
-          case .NonConditionalRetriable:
-            strongSelf.executionDelegate?.fail(pTask: pTask, increaseRetryCount: true)
-            strongSelf.changeManager?.didComplete(task: pTask.task, result: result) // Possible in only certain cases
-          }
+        case .ConditionalRetriable:
+          strongSelf.executionDelegate?.fail(pTask: pTask, increaseRetryCount: false)
+          return
+        case .NonConditionalRetriable:
+          strongSelf.executionDelegate?.fail(pTask: pTask, increaseRetryCount: true)
+          strongSelf.changeManager?.didComplete(task: pTask.task, result: result) // Possible in only certain cases
         }
       }
     }
