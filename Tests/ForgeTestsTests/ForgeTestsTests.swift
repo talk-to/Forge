295f0582d8f585a6a7571eb91b0b7941604dc702
Ayush Goel <ayushgoel111@gmail.com>
2019-01-08 19:45:13 +0530

    Add test case for normal functioning of Forge

--- Tests/ForgeTestsTests/ForgeTestsTests.swift
+++ Tests/ForgeTestsTests/ForgeTestsTests.swift
@@ -9,19 +9,79 @@
 import XCTest
 @testable import ForgeTests
 @testable import Forge
+import Result
+
+class TestExpectation: XCTestExpectation {
+  private var didFulfill = false
+
+  var isFulfilled: Bool {
+    return didFulfill
+  }
+
+  override func fulfill() {
+    super.fulfill()
+    didFulfill = true
+  }
+}
+
+class TestChangeManager: ChangeManager {
+  var willStartCalledTimes = 0
+  func willStart(task: Task) {
+    willStartCalledTimes += 1
+  }
+
+  var didCompleteCalledTimes = 0
+  func didComplete(task: Task, result: Result<Bool, ExecutorError>) {
+    didCompleteCalledTimes += 1
+  }
+}
+
+class TestExecutor: Executor {
+  var executedTasks = 0
+  func execute(task: Task, countOfRetries: Int, completion: @escaping (Result<Bool, ExecutorError>) -> Void) {
+    executedTasks += 1
+    completion(Result(value: true))
+  }
+}
 
 class ForgeTestsTests: XCTestCase {
 
-  override func setUp() {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
-
   func testForgeCanBeInitialised() {
     XCTAssertNotNil(Forge(with: "test"))
+  }
+
+  func testRunningOfATask() {
+    let forge = Forge(with: "test")
+    let changeManager = TestChangeManager()
+    forge.changeManager = changeManager
+    let executor = TestExecutor()
+    try! forge.register(executor: executor, for: "t")
+    let task = Task(id: "id", type: "t")
+    forge.submit(task: task)
+
+    let e1 = TestExpectation(description: "changeManager will start is called")
+    let e2 = TestExpectation(description: "changeManager did complete is called")
+    let e3 = TestExpectation(description: "executor called")
+    var times = 0
+    while true {
+      if changeManager.willStartCalledTimes == 1 {
+        e1.fulfill()
+      }
+      if changeManager.didCompleteCalledTimes == 1 {
+        e2.fulfill()
+      }
+      if executor.executedTasks == 1 {
+        e3.fulfill()
+      }
+      if e1.isFulfilled && e2.isFulfilled && e3.isFulfilled {
+        break
+      }
+      RunLoop.current.run(until: Date(timeIntervalSinceNow: 1))
+      times += 1
+      if (times >= 1) {
+        XCTFail()
+      }
+    }
   }
 
 
