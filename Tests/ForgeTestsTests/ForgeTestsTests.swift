761a616721c5440f1f95b4d8e409759cafc37568
Ayush Goel <ayushgoel111@gmail.com>
2019-01-08 16:55:23 +0530

    Create test target with host application
    With new build system in Xcode 10, a test target without a host
    was not building. Xcode was unable to load the test bundle. This fixes it
    by creating a host app.

--- Tests/ForgeTestsTests/ForgeTestsTests.swift
+++ Tests/ForgeTestsTests/ForgeTestsTests.swift
@@ -1,25 +1,33 @@
-import XCTest
-import Forge
+//
+//  ForgeTestsTests.swift
+//  ForgeTestsTests
+//
+//  Created by Ayush Goel on 1/8/19.
+//  Copyright © 2019 Flock. All rights reserved.
+//
 
-class Tests: XCTestCase {
+import XCTest
+@testable import ForgeTests
+@testable import Forge
+
+class ForgeTestsTests: XCTestCase {
 
   override func setUp() {
-    super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
   }
 
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
   }
 
   func testForgeCanBeInitialised() {
     XCTAssertNotNil(Forge(with: "test"))
   }
 
+
   func testPerformanceExample() {
     // This is an example of a performance test case.
-    self.measure() {
+    self.measure {
       // Put the code you want to measure the time of here.
     }
   }
