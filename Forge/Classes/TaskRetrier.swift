0e3cfce9c1767c85ba7daccf1905f8a29f955906
Ayush Goel <ayushgoel111@gmail.com>
2019-01-16 20:44:35 +0530

    Add task retier to retry tasks every second

---
+++ Forge/Classes/TaskRetrier.swift
@@ -0,0 +1,41 @@
+
+import Foundation
+
+private let ExecutionTimeGap = 1
+
+class TaskRetrier {
+
+  private let executionManager: ExecutionManager
+  private let persistor: Persistor
+
+  init(executionManager: ExecutionManager, persistor: Persistor) {
+    self.executionManager = executionManager
+    self.persistor = persistor
+  }
+
+  private var repeatingTimer: Timer?
+
+  func startRetrialForTasks() {
+    if repeatingTimer == nil {
+      repeatingTimer = Timer.scheduledTimer(withTimeInterval: TimeInterval(ExecutionTimeGap), repeats: true) { [weak self] t in
+        guard let strongSelf = self else {
+          print("Retrier deallocated, stopping..")
+          return
+        }
+        strongSelf.executeTasks()
+      }
+    }
+  }
+
+  deinit {
+    repeatingTimer?.invalidate()
+  }
+
+  private func executeTasks() {
+    persistor.tasks(ofType: "") // FIXME: Get tasks with elapsed wait time
+      .forEach { pTask in
+        executionManager.safeExecute(task: pTask)
+    }
+  }
+
+}
