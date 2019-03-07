f8493f7c5fcee7207811657465c44e54c583ec34
Dinesh Kumar <dinesh.ku@flock.com>
2019-03-07 15:49:19 +0530

    Allow result type to be Result<Any, ExecutorError> instead of Result<Bool, ExecutorError>

--- Forge/Classes/Executor.swift
+++ Forge/Classes/Executor.swift
@@ -3,6 +3,6 @@ import Result
 
 public protocol Executor {
 
-  func execute(task: Task, countOfRetries: Int, completion: @escaping (Result<Bool, ExecutorError>) -> Void)
+  func execute(task: Task, countOfRetries: Int, completion: @escaping (Result<Any, ExecutorError>) -> Void)
 
 }
